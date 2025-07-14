0eee1e9ea4b82d4bf2dacf176f4bf877413436e6
GautamMIH <gautam.mudbhari16@gmail.com>
2025-07-14 14:44:40 +0545

    Removed Files

--- hook-pynput.py
+++
@@ -1,4 +0,0 @@
-# hook-pynput.py
-from PyInstaller.utils.hooks import collect_submodules
-hiddenimports = collect_submodules('pynput.keyboard._win32')
-hiddenimports += collect_submodules('pynput.mouse._win32')
